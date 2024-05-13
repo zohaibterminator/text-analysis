0786184d29ac2365cce8a55b5bfc4ea017394e28
Mohammad Zohaib Saqib <117584067+zohaibterminator@users.noreply.github.com>
2024-05-13 22:15:57 +0500

    Add files via upload

--- Homepage.py
+++ Homepage.py
@@ -6,12 +6,12 @@ st.set_page_config(
     page_icon="📚",
 )
 
-st.title("Text-Classifier and Text-Clustering")
+st.title("Document-Classifier and Document-Clustering")
 
 st.subheader("Welcome to the App!")
 st.markdown("This app is designed to demonstrate two text analysis techniques: Document Classification and Document Clustering")
 st.markdown("- Document Classification: Document classification involves categorizing documents into predefined classes or categories based on their content. This task is often supervised, meaning that the classification model is trained on labeled data, where each document is associated with its correct class or category. The app uses the K-Nearest Neighbors algorithm to classify the text documents.")
-st.markdown("- Document Clustering: Document clustering, on the other hand, is an unsupervised task where documents are grouped into clusters based on their similarity. Clustering algorithms identify natural groupings within the documents without any prior knowledge of class labels.")
+st.markdown("- Document Clustering: Document clustering, on the other hand, is an unsupervised task where documents are grouped into clusters based on their similarity. Clustering algorithms identify natural groupings within the documents without any prior knowledge of class labels. The app uses the K-Means algorithm to cluster the text documents.")
 
 st.markdown('''
 <style>
